dc21931970c054537480e0f7d1ca412eaf5a4736
vijay mali <guddavijay@gmail.com>
2016-10-10 15:40:40 +0530

    mariadb 10.1 for ubuntu 16.04

--- distros/ubuntu-16.04/install_mysql.sh
+++ distros/ubuntu-16.04/install_mysql.sh
@@ -18,6 +18,9 @@ InstallSQLServer() {
     echo -n "Installing MariaDB... "
     echo "mysql-server-5.5 mysql-server/root_password password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
     echo "mysql-server-5.5 mysql-server/root_password_again password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
+    apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys F1656F24C74CD1D8
+    sudo add-apt-repository 'deb [arch=amd64,i386] http://sgp1.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu xenial main'
+    sudo apt-get update
     apt-get -y install mariadb-client mariadb-server > /dev/null 2>&1
     sed -i 's/bind-address		= 127.0.0.1/#bind-address		= 127.0.0.1/' /etc/mysql/mariadb.conf.d/50-server.cnf
     service mysql restart /dev/null 2>&1
